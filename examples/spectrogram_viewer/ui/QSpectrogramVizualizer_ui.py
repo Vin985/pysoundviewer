434388b44c8a34bc8a067a4a1849b3cfdd1be9e2
Vin985 <sylvain.christin@gmail.com>
2022-11-04 15:51:21 -0300

    pyside 6

--- examples/spectrogram_viewer/ui/QSpectrogramVizualizer_ui.py
+++ examples/spectrogram_viewer/ui/QSpectrogramVizualizer_ui.py
@@ -3,20 +3,25 @@
 ################################################################################
 ## Form generated from reading UI file 'QSpectrogramVizualizer.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.2
+## Created by: Qt User Interface Compiler version 6.4.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QSizePolicy,
+    QVBoxLayout, QWidget)
 
-from pysoundplayer.gui.QSoundPlayer import QSoundPlayer
 from pysoundplayer.gui.QImageOptions import QImageOptions
-from pysoundplayer.gui.QSpectrogramViewer import QSpectrogramViewer
+from pysoundplayer.gui.QSoundPlayer import QSoundPlayer
 from pysoundplayer.gui.QSpectrogramOptions import QSpectrogramOptions
-
+from pysoundplayer.gui.QSpectrogramViewer import QSpectrogramViewer
 
 class Ui_QSpectrogramVizualizer(object):
     def setupUi(self, QSpectrogramVizualizer):
--- examples/spectrogram_viewer_example_ui.py
+++ examples/spectrogram_viewer_example_ui.py
@@ -3,20 +3,26 @@
 ################################################################################
 ## Form generated from reading UI file 'spectrogram_viewer_example.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.2
+## Created by: Qt User Interface Compiler version 6.4.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QMainWindow,
+    QMenuBar, QSizePolicy, QStatusBar, QVBoxLayout,
+    QWidget)
 
 from pysoundplayer.widget.sound_player_widget import SoundPlayerWidget
 from pysoundviewer.gui.widgets.image_options_widget import ImageOptionsWidget
-from pysoundviewer.gui.widgets.spectrogram_viewer import SpectrogramViewer
 from pysoundviewer.gui.widgets.spectrogram_options_widget import SpectrogramOptionsWidget
-
+from pysoundviewer.gui.widgets.spectrogram_viewer import SpectrogramViewer
 
 class Ui_SpectrogramViewerExample(object):
     def setupUi(self, SpectrogramViewerExample):
